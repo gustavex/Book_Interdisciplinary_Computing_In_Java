2fa509e2cc98f31911be2aa105d974f3bc6e5730
Senocosen0g <cyber_gustavo@hotmail.com>
2019-10-25 22:23:50 +0900

    update20191025

---
+++ Chapter02/src/MyWindow.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.io.*;
+import java.lang.*;
+import java.awt.event.*;
+import java.awt.print.*;
+import java.awt.print.PrinterJob;
+import java.text.DecimalFormat;
+
+public class MyWindow extends Frame implements ActionListener, Printable{
+	Plotter plotting;
+	int nColns, nLines, xIndex, nSkips;
+	double[][] Table;
+	boolean beforePlot = true;
+	
+	public sta
+	
+}
